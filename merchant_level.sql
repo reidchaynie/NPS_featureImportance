c1c68ea4d75bc6e07f95d2e2ad3b6a15e19ce754
Reid Haynie <91569472+reidchaynie@users.noreply.github.com>
2024-06-10 19:48:32 -0500

    Create merchant_level.sql

---
+++ merchant_level.sql
@@ -0,0 +1,165 @@
+with first_purchase_products_2022 as (
+select 
+  order_id 
+  ,listagg(distinct LINE_ITEM_DERIVED_DETAILS.MERCHANT_ID,',') as MERCHANT_ID_
+ ,listagg(distinct LINE_ITEM_DERIVED_DETAILS.MERCHANT_name,',') as MERCHANT_name_
+  ,purchasing_household_id  
+  ,min(order_placed) as first_order_placed
+from 
+  PROD_MART_GOLDBELLY_DB.ANALYTICS_MART.LINE_ITEM_DERIVED_DETAILS
+where 
+  is_first_order 
+  and year(order_placed) = 2022
+  and month(order_placed) < 9
+  and product_id not in (26945, 24586) -- removes gift card purchases, which is a tremendous outlier that skews the data 
+  and LINE_ITEM_DERIVED_DETAILS.merchant_id in 
+    (select id from PROD_MART_GOLDBELLY_DB.ANALYTICS_MART.merchants where is_featured and is_Available) -- narrows to currently active products  
+  and not IS_SUBSCRIPTION_OG and not IS_SUBSCRIPTION_FULFILLMENT
+  and not IS_CORP_ORDER
+and not IS_CANCELED 
+group by 1,4
+having 
+  merchant_id_ not like '%,%' --removes orders containing multiple products
+),
+-- second CTE gets counts of first-time purchasers for each product in the first CTE range 
+-- removes all products that dont have at least 200 first-time purchasers 
+counts_of_first_Purchase_by_product_2022 as (
+  select 
+    MERCHANT_name_ 
+    ,1 as join_on_me -- the next CTE joins the same value to every record in this CTE, so I am using this join key 
+    ,count(distinct PURCHASING_HOUSEHOLD_ID) as num_first_Time_purchase 
+  from
+    first_purchase_products_2022 
+  group by 1,2
+  having num_first_Time_purchase >= 200 -- this removes products beneath a min volume threshold. If an order has 80% retention on 5 orders, who cares
+),
+-- this CTE is getting the max number of first-time purchasers of any product so that we can scale products against it 
+-- using the join_on_me field to join this total to every product 
+first_time_factor as (
+  select 
+        1 as join_on_me
+        ,max(num_first_time_purchase) as max_purchases -- the most first-time purchases belonging to a product 
+  from
+    counts_of_first_Purchase_by_product_2022
+  group by 1 
+),
+-- this CTE is taking max_purchases and dividing it by the count of first_time_purchasers for each product
+-- this is scaling the products against each other by their number of first-time purchasers 
+volume_Factor_calc_2022 as (
+  select 
+      MERCHANT_name_
+      ,num_first_Time_purchase
+      ,(num_first_Time_purchase/max_purchases) 
+          as first_Time_purchase_factor  
+  from 
+    counts_of_first_Purchase_by_product_2022
+  left join 
+    first_time_factor on counts_of_first_Purchase_by_product_2022.join_on_me = first_Time_factor.join_on_me
+),
+-- next cte pulls every order for all customers identified in the first CTE
+-- this cte adopts all constraints from the first CTE, so it only needs the single where condition
+follow_up_counts_setup as (
+  select 
+    purchasing_household_id 
+    ,order_id
+    ,min(order_placed) as order_Placed 
+    from
+    PROD_MART_GOLDBELLY_DB.ANALYTICS_MART.LINE_ITEM_DERIVED_DETAILS
+  where 
+    PURCHASING_HOUSEHOLD_ID  in 
+      (select PURCHASING_HOUSEHOLD_ID from first_purchase_products_2022)
+  group by 1,2
+),
+-- next cte is calculating the end range for each phh (8 months after their first purchase) 
+-- this is so we can compare records from 2022 apples:apples with records in 2023 
+order_range as (
+  select 
+    PURCHASING_HOUSEHOLD_ID
+    ,min(order_placed) as first_order_placed
+    ,dateadd('month',8,first_order_placed) as end_of_range
+  from
+    follow_up_counts_setup
+  group by 1 
+),
+-- this CTE sees which phh are one_and_done and which are retained 
+follow_up_counts as (
+  select 
+    follow_up_counts_setup.PURCHASING_HOUSEHOLD_ID 
+    ,count(distinct order_id) - 1 as total_follow_up_orders -- minus 1 becuase we dont want to count the initial purchase 
+    ,case when total_follow_up_orders = 0 then true else false end as is_oad -- if 0 then no follow-up purchases: One-and-done (oad)
+  from
+    follow_up_counts_setup
+  join 
+    order_range on follow_up_counts_setup.purchasing_household_id = order_range.purchasing_household_id 
+  where 
+    order_placed <= end_of_range -- limits to first 6 months 
+  group by 1
+  ),
+-- counts the number of retained customers for every product
+retention_counts as (
+  select 
+    merchant_id_
+    ,MERCHANT_name_
+    ,count(follow_up_counts.purchasing_household_id) as total_purchasers -- this is to limit by 200 first time purchasers like we did above  
+    ,count(distinct case when is_oad then follow_up_counts.purchasing_household_id end) as total_oad 
+    ,count(distinct case when not is_oad then follow_up_counts.purchasing_household_id end) as total_retained 
+  from 
+    first_purchase_products_2022
+  left join
+    follow_up_counts on first_purchase_products_2022.purchasing_household_id = follow_up_counts.purchasing_household_id 
+  group by 1,2
+  having total_purchasers >= 200 
+),
+--poorly titled cte brings data together and calculates percent retained 
+final as (
+select 
+  1 as join_on_me 
+  ,retention_counts.MERCHANT_name_
+  ,retention_counts.merchant_id_
+  ,total_oad 
+  ,total_Retained  
+  ,first_Time_purchase_factor
+  ,num_first_Time_purchase
+  ,total_retained/num_first_Time_purchase as pct_retained
+
+from 
+  retention_counts 
+left join volume_Factor_calc_2022 on retention_counts.MERCHANT_name_ = volume_Factor_calc_2022.MERCHANT_name_
+)
+,
+-- here we are calculating max retention rate, just like we calculated max purchase volume. 
+-- This is so we can scale products against eachother based on retention
+max_retained as (
+  select 
+    1 as join_on_me 
+    ,max(pct_retained) as max_retention 
+  from 
+    final 
+  group by 1 
+),
+-- poorly titled CTE2 to calculate retention factor 
+final_ as (
+  select 
+    final.* 
+    ,max_Retention
+    ,(pct_retained/max_retention) as retention_factor 
+from
+  final 
+  left join max_retained on final.join_on_me = max_retained.join_on_me
+)
+
+
+
+-- output
+-- final score calc 
+select 
+  MERCHANT_id_
+  ,MERCHANT_name_  
+  ,num_first_time_purchase
+  ,total_Retained 
+  ,pct_retained
+  ,first_time_purchase_factor 
+  ,retention_factor
+  ,round((first_time_purchase_factor + (retention_factor+retention_factor))/3,2)*100 as score_  
+from 
+  final_
